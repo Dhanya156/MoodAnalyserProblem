d438638904df011f65eff7b8c054eddb17aa4d44
Dhanya S V <dhanyavirupaksha@gmail.com>
2020-11-01 15:42:32 +0530

    Added : Custom Exceptions To Handle InvalidMood

--- MoodAnalyserUnitTestCases/MoodAnalyserTestCases.cs
+++ MoodAnalyserUnitTestCases/MoodAnalyserTestCases.cs
@@ -1,27 +1,44 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MoodAnalyserProblem;
+using System;
+using System.Runtime.Serialization;
 
 namespace MoodAnalyserUnitTestCases
 {
     [TestClass]
     public class MoodAnalyserTestCases
     {
+        ///TC 3.1 Given null mood throw MoodAnalysis Exception 
         [TestMethod]
-        [DataRow(null)]
-        ///TC 2.1 Given message Null should Return Happy 
-        /// <param name="message">The message.</param>
-        public void Given_Message_NULL_Should_Return_HAPPY(string message)
+        public void Given_NULL_Mood_Should_throw_MoodAnalysisException()
         {
-            ///Arrange
-            string expected = "HAPPY";
-            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+            try
+            {
+                string message = null;
+                MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+                string mood = moodAnalyser.AnalyseMood();
+            }
+            catch (AnalyserCustomExceptions e)
+            {
+                Assert.AreEqual("Mood should not be null", e.Message);
+            }
+        }
 
-            ///Act
-            string mood = moodAnalyser.AnalyseMood();
 
-            ///Assert
-            Assert.AreEqual(expected, mood);
-
+        ///TC 3.2 Given empty mood throw MoodAnalysis Exception 
+        [TestMethod]
+        public void Given_Empty_Mood_Should_throw_MoodAnalysisException()
+        {
+            try
+            {
+                string message = ""; 
+                MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+                string mood = moodAnalyser.AnalyseMood();
+            }
+            catch(AnalyserCustomExceptions e)
+            {
+                Assert.AreEqual("Mood should not be Empty", e.Message);
+            }
         }
     }
 }
