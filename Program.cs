677d8103c6949cda6d2e1bd982b9adff3accc459
Dhanya S V <dhanyavirupaksha@gmail.com>
2020-11-01 11:22:08 +0530

    Added : Welcome Message

--- Program.cs
+++ Program.cs
@@ -4,9 +4,11 @@ namespace MoodAnalyserProblem
 {
     class Program
     {
+        /// <summary>Defines the entry point of the application.</summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Mood Analyser Problem");
         }
     }
 }
