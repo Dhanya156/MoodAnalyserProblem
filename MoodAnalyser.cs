64b807fe6689ef613a028d8a86c61d064d8ad17e
Dhanya S V <dhanyavirupaksha@gmail.com>
2020-11-01 12:48:19 +0530

    Added : Method Ability To Analyse Mood With Test Cases

---
+++ MoodAnalyser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyserProblem
+{
+    /// <summary>
+    ///MoodAnalyser class is created to Analyse the Mood
+    /// </summary>
+    public class MoodAnalyser
+    {
+        /// <summary>The message</summary>
+        private string message;
+        /// <summary>Initializes a new instance of the <see cref="MoodAnalyser" /> class.</summary>
+        public MoodAnalyser()
+        {
+
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="MoodAnalyser" /> class.</summary>
+        /// <param name="message">The message.</param>
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        /// <summary>Analyses the mood.</summary>
+        /// <param name="message">The message.</param>
+        /// <returns>
+        ///AnalyseMood Is a method that returns Happy or Sad
+        /// </returns>
+        public string AnalyseMood(string message)
+        {
+            if (message.Contains("sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        }
+
+        public string AnalyseMood()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
